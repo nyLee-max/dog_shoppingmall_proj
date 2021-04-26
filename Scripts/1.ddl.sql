ff9d22ea06e78b33a1b64f72e37ae72f97aca45e
nyLee <skdus2421@naver.com>
2021-04-26 10:42:24 +0900

    기본 프로젝트 작업 완료 데이터베이스 작업 완료

---
+++ Scripts/1.ddl.sql
@@ -0,0 +1,32 @@
+-- 개
+DROP TABLE IF EXISTS shoppingmall.Dog RESTRICT;
+
+-- 개_쇼핑몰
+DROP SCHEMA IF EXISTS shoppingmall;
+
+-- 개_쇼핑몰
+CREATE SCHEMA shoppingmall;
+
+-- 개
+CREATE TABLE shoppingmall.Dog (
+	id        INT         NOT NULL COMMENT '아이디', -- 아이디
+	kind      VARCHAR(12) NOT NULL COMMENT '종류', -- 종류
+	price     INT         NOT NULL COMMENT '가격', -- 가격
+	image     VARCHAR(20) NOT NULL COMMENT '개 이미지', -- 개 이미지
+	country   VARCHAR(12) NULL     COMMENT '원산지', -- 원산지
+	height    INT         NULL     COMMENT '개신장', -- 개신장
+	weight    INT         NULL     COMMENT '개체중', -- 개체중
+	content   VARCHAR(20) NULL     COMMENT '개 설명', -- 개 설명
+	readcount INT         NULL     COMMENT '조회수' -- 조회수
+)
+COMMENT '개';
+
+-- 개
+ALTER TABLE shoppingmall.Dog
+	ADD CONSTRAINT PK_Dog -- 개 기본키
+		PRIMARY KEY (
+			id -- 아이디
+		);
+
+ALTER TABLE shoppingmall.Dog
+	MODIFY COLUMN id INT NOT NULL AUTO_INCREMENT COMMENT '아이디';
---
+++ Scripts/Script.sql
@@ -0,0 +1,3 @@
+grant all
+   on shoppingmall.*
+   to 'user_shoppingmall'@'localhost' identified by 'rootroot';
